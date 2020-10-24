2b56487bac159cbe4b424ba8f971403e645b95d0
Robert Garcia <robgarcia552@gmail.com>
2020-10-24 12:11:33 -0400

    initial commit

---
+++ Controllers/ConferenceController.cs
@@ -0,0 +1,17 @@
+﻿using Globalmantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globalmantics.Controllers
+{
+    public class ConferenceController: Controller
+    {
+        public ConferenceController(IConferenceService conferenceService)
+        {
+
+        }
+    }
+}
---
+++ Services/ConferenceApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globalmantics.Services
+{
+    public class ConferenceApiService: IConferenceService
+    {
+    }
+}
